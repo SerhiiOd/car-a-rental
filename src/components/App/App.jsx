ab1796391e3931f130db8f2a42b7f38d60348c50
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-10-09 04:29:12 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import SharedLayout from 'components/SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../../pages/Home/Home'));
 const Catalog = lazy(() => import('../../pages/Catalog/Catalog'));
-const Favorites = lazy(() => import('../../pages/Favorites/Favorites'));
+// const Favorites = lazy(() => import('../../pages/Favorites/Favorites'));
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
-          <Route path="/favorites" element={<Favorites />} />
+          {/* <Route path="/favorites" element={<Favorites />} /> */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
